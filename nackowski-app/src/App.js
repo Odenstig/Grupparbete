0c105238b99213f2287ab11083e48c90bb22af92
Carl Munter <91054492+CharlieBooy@users.noreply.github.com>
2022-04-05 11:13:15 +0200

    second

--- nackowski-app/src/App.js
+++ nackowski-app/src/App.js
@@ -3,13 +3,8 @@ import './App.css';
 
 function App() {
   return (
-<<<<<<< HEAD
     <p>Hej</p>
-=======
-    <div className="App">
-      Hejhopp!
-    </div>
->>>>>>> 40085aa6d6f7d554e9eb243ccaa35a15229f0f33
+    
   );
 }
 
