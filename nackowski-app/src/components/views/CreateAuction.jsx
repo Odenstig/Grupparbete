aaade4580e219191f152a1f219d994121a4e92af
Zerthz <56971417+Zerthz@users.noreply.github.com>
2022-04-06 21:11:37 +0200

    Added refs to createauction for the fields

--- nackowski-app/src/components/views/CreateAuction.jsx
+++ nackowski-app/src/components/views/CreateAuction.jsx
@@ -1,27 +1,41 @@
 
-import React from 'react'
-import {Button} from 'react-bootstrap'
+import React, { useRef } from 'react'
+import { Button } from 'react-bootstrap'
 import Form from 'react-bootstrap/Form'
 
 const CreateAuction = () => {
+  const title = useRef();
+  const price = useRef();
+  const name = useRef();
+  const endDate = useRef();
+  const description = useRef();
+
+
+
+
+
   return (
     <Form >
 
       <Form.Label>Titel</Form.Label>
-      <Form.Control  placeholder='Titel'/>
-      <br/>
+      <Form.Control placeholder='Titel' ref={title} />
+      <br />
+
+      <Form.Label>Beskrivning</Form.Label>
+      <Form.Control as="textarea" placeholder='Beskrivning' rows={4} ref={description} />
+      <br />
 
       <Form.Label>Pris</Form.Label>
-      <Form.Control  placeholder='Pris'/>
-      <br/>
+      <Form.Control placeholder='Pris' ref={price} />
+      <br />
 
       <Form.Label>Skapare</Form.Label>
-      <Form.Control  placeholder='Skapare'/>
-      <br/>
+      <Form.Control placeholder='Skapare' ref={name} />
+      <br />
 
       <Form.Label>Slutdatum</Form.Label>
-      <Form.Control type="date"  placeholder='Slutdatum'/>
-      <br/>
+      <Form.Control type="date" placeholder='Slutdatum' ref={endDate} />
+      <br />
 
       <Button type='submit'>Skapa auktion</Button>
 
