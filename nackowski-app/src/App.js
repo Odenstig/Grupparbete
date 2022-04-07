f9d275b4c05dab3949600586c5725f2823e1c0f7
Albin Dragsten <albin.dragsten@gmail.com>
2022-04-07 13:27:37 +0200

    asd

--- nackowski-app/src/App.js
+++ nackowski-app/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Main from './components/navigation/Main';
 import Navigation from './components/navigation/Navigation';
+import Search from './components/Search';
 
 function App() {
   return (
     <>
       <Navigation />
       <Main />
+      <Search />
     </>
 
 
