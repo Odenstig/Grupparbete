062b66a5e4d7f46d773107a005ca1b30200e2113
Albin Dragsten <albin.dragsten@gmail.com>
2022-04-07 11:14:25 +0200

    Merge branch 'Development' of https://github.com/Odenstig/Grupparbete into Albin

---
+++ nackowski-app/src/components/BidList.jsx
@@ -0,0 +1,15 @@
+import {useRef} from 'react';
+import React from 'react';
+
+
+const BidList = () => {
+
+    let urlAuktion = "http://nackowskis.azurewebsites.net/api/Auktion/2400";
+    let urlBid = "http://nackowskis.azurewebsites.net/api/bud/2400/"
+
+    fetch(url)
+    .then(response => response.json())
+    .then()
+};
+
+export default BidList;
