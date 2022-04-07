976ed96ef8ab28f1a27bcff2dc890f12552e87d6
Felix <56971417+Zerthz@users.noreply.github.com>
2022-04-07 11:13:18 +0200

    After creating auction now brings you back to home page

--- nackowski-app/src/components/views/CreateAuction.jsx
+++ nackowski-app/src/components/views/CreateAuction.jsx
@@ -1,7 +1,8 @@
-
+import dayjs from 'dayjs';
 import React, { useRef } from 'react'
 import { Button, Container } from 'react-bootstrap'
 import Form from 'react-bootstrap/Form'
+import { useNavigate } from 'react-router-dom';
 
 const CreateAuction = () => {
   const title = useRef();
@@ -9,15 +10,44 @@ const CreateAuction = () => {
   const name = useRef();
   const endDate = useRef();
   const description = useRef();
+  const navigate = useNavigate();
 
 
+  let dateNow = dayjs();
+
+  const postAuction = (e) => {
+    let auction = {
+      "Titel": title.current.value,
+      "Beskrivning": description.current.value,
+      "StartDatum": dateNow,
+      "SlutDatum": endDate.current.value,
+      "Gruppkod": 2400,
+      "Utropspris": price.current.value,
+      "SkapadAv": name.current.value,
+    }
+
+    let url = "http://nackowskis.azurewebsites.net/api/Auktion/2400";
+
+    fetch(url, {
+      method: 'POST',
+      body: JSON.stringify(auction),
+      headers: {
+        'Accept': 'application/json, text/plain, */*',
+        'Content-Type': 'application/json'
+      }
+    })
+      .then(data => {
+        console.log(data + ' posten är skapad');
+      })
+      .catch(err => console.log(err));
+    navigate('/');
+  }
 
 
 
   return (
     <Container className="justify-content-md-center">
-
-      <Form>
+      <Form id="form">
 
         <Form.Label>Titel</Form.Label>
         <Form.Control placeholder='Titel' ref={title} size="lg" />
@@ -39,11 +69,11 @@ const CreateAuction = () => {
         <Form.Control type="date" placeholder='Slutdatum' ref={endDate} />
         <br />
 
-        <Button type='submit'>Skapa auktion</Button>
+        <Button onClick={postAuction}>Skapa auktion</Button>
 
       </Form>
 
-    </Container>
+    </Container >
   );
 };
 
--- nackowski-app/src/containers/AuctionContainer.jsx
+++ nackowski-app/src/containers/AuctionContainer.jsx
@@ -1,23 +1,23 @@
-import React,{useEffect,useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import AuctionList from '../components/AuctionList';
 import Search from '../components/Search';
 
 const AuctionContainer = () => {
 
-    const[auctionList,setAuctionList] = useState([]);
+    const [auctionList, setAuctionList] = useState([]);
 
-    useEffect(()=>{
+    useEffect(() => {
         let url = "http://nackowskis.azurewebsites.net/api/Auktion/2400/"
 
         fetch(url)
-        .then(res=>res.json())
-        .then(data =>{
-            console.log(data)
-            setAuctionList(data);
-        })
-    },[]);
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+                setAuctionList(data);
+            })
+    });
 
-    const search = (searchparam)=>{
+    const search = (searchparam) => {
         // let url ="https://swapi.dev/api/people/?search=" + searchparam;
 
         // fetch(url)
@@ -28,12 +28,12 @@ const AuctionContainer = () => {
         // });
     };
 
-  return (
-      <>
-      <Search callback = {search}/>
-      <AuctionList list = {auctionList}/>
-      </>    
-  );
+    return (
+        <>
+            <Search callback={search} />
+            <AuctionList list={auctionList} />
+        </>
+    );
 };
 
 export default AuctionContainer;
