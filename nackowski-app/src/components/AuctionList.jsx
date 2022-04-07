4ae3edc8132cb3e05c43c9268378e90534f728d4
Felix <56971417+Zerthz@users.noreply.github.com>
2022-04-07 13:27:37 +0200

    Now sorts in the correct order, by Auction EndDate

--- nackowski-app/src/components/AuctionList.jsx
+++ nackowski-app/src/components/AuctionList.jsx
@@ -15,28 +15,33 @@ const AuctionList = ({ list }) => {
         margin: "20px",
         float: "left"
     }
-
+    list = list.sort((a, b) => {
+        console.log(a, b)
+        return (dayjs(b.SlutDatum).isAfter(dayjs(a.SlutDatum)) ? 1 : -1);
+    })
     let auctionList = list.map(auction => {
         let endDate = dayjs(auction.SlutDatum).format("YYYY-MM-DD HH:mm")
         return (
             <div Class="container-md-2">
 
-            <Card style={card}>
-                <Card.Header>
-                    <Card.Title>{auction.Titel}<span style={right}>{endDate}</span> </Card.Title>
-                </Card.Header>
-                <Card.Body>
-                    <Card.Text>{auction.Beskrivning}</Card.Text>
-                </Card.Body>
-                <Card.Footer>
-                    <Card.Text>{auction.SkapadAv}<span style={right}>{auction.Utropspris}</span></Card.Text>
-                </Card.Footer>
+                <Card style={card}>
+                    <Card.Header>
+                        <Card.Title>{auction.Titel}<span style={right}>{endDate}</span> </Card.Title>
+                    </Card.Header>
+                    <Card.Body>
+                        <Card.Text>{auction.Beskrivning}</Card.Text>
+                    </Card.Body>
+                    <Card.Footer>
+                        <Card.Text>{auction.SkapadAv}<span style={right}>{auction.Utropspris}</span></Card.Text>
+                    </Card.Footer>
 
-                <Button className='btn btn-dark'>Mer</Button>
-            </Card>
+                    <Button className='btn btn-dark'>Mer</Button>
+                </Card>
             </div>
         );
     });
+
+
     return (
         <div>{auctionList}</div>
     );
