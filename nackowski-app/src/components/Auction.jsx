c19c7f6def791ec5a07cbb24a344aac8bcb08a0d
Carl Munter <91054492+CharlieBooy@users.noreply.github.com>
2022-05-19 13:27:28 +0200

    small letters

--- nackowski-app/src/components/Auction.jsx
+++ nackowski-app/src/components/Auction.jsx
@@ -27,21 +27,21 @@ const Auction = ({ auction, endDate, handleClick, removeAuction, handleUpdateCli
 
       <Card style={card}>
         <Card.Header>
-          <Card.Title>{auction.Titel}</Card.Title>
+          <Card.Title>{auction.titel}</Card.Title>
           <Card.Title><span style={right}>{endDate}</span></Card.Title>
         </Card.Header>
         <Card.Body>
           {auction.aktiv === "Aktiv" ? <div className='card-price2'>
-            <Card.Text >Utropspris:   {auction.Utropspris}kr</Card.Text>
+            <Card.Text >Utropspris:   {auction.utropspris}kr</Card.Text>
           </div>
             :
             <div className='card-price'>
-              <Card.Text >Utropspris:   {auction.Utropspris}kr</Card.Text>
+              <Card.Text >Utropspris:   {auction.utropspris}kr</Card.Text>
             </div>}
-          <Card.Text style={mid} >{auction.Beskrivning}</Card.Text>
+          <Card.Text style={mid} >{auction.beskrivning}</Card.Text>
         </Card.Body>
         <Card.Footer>
-          <Card.Text>{auction.SkapadAv}<span style={right}>{auction.aktiv}</span></Card.Text>
+          <Card.Text>{auction.UserID}<span style={right}>{auction.aktiv}</span></Card.Text>
         </Card.Footer>
         <div className='container-md-3'>
           <Button style={btn} className='btn btn-dark' onClick={() => handleClick(auction)} >Detaljer</Button>
--- nackowski-app/src/containers/AuctionContainer.jsx
+++ nackowski-app/src/containers/AuctionContainer.jsx
@@ -12,7 +12,7 @@ const AuctionContainer = () => {
     const [loader, setLoaderState] = useState(true);
 
     useEffect(() => {
-        let url = "https://localhost:7203/api/auction"
+        let url = "https://nackowskiapiapi20220519103545.azurewebsites.net/api/auction"
 
         const getData = async () => {
             await fetch(url)
